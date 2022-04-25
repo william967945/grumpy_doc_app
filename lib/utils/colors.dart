66b9c78df5650f677a9556c9b61fbb13b3b15707
william967945 <william967945@yahoo.com>
2022-04-25 14:48:30 +0800

    add colors.dart

---
+++ lib/utils/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const mainThemeColor = Color(0xFF414D6B);
+const textColor = Color(0xFFFFFFFF);
+Color homePageButtonColor = Color(0xFFFFFFFF).withOpacity(0.83);
